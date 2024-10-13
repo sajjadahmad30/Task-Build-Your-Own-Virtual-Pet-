59be53798fdbf012211873fd5659a24350d27719
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-10-13 09:20:50 +0500

    create function name feed() and play().

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 let pet = {
-    name: "Fluffy",
-    type: "cat",
+    name: "Jakey 🐕‍🦺",
+    type: "dog",
     age: 1,
     happiness: 50,
     hunger: 30,
   
+    feed: function() {
+        this.hunger -= 20;
+        if (this.hunger < 0) {
+          this.hunger = 0;
+        }
+      },
     
+      play: function() {
+        this.happiness += 20;
+        if (this.happiness > 100) {
+          this.happiness = 100;
+        }
+      },
+    
+     
   }
